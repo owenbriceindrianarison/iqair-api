a391844f00b6c4631f999b9eaa899ca2912ce57a
Owen Brice Indrianarison <indrianarison.brice@gmail.com>
2021-11-30 11:34:53 +0100

    test

--- src/pollution/controllers/pollution.controller.spec.ts
+++ src/pollution/controllers/pollution.controller.spec.ts
@@ -1,18 +1,55 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PollutionService } from '../services/pollution.service';
 import { PollutionController } from './pollution.controller';
 
 describe('PollutionController', () => {
   let controller: PollutionController;
 
+  const mockPollutionService = {
+    getPollution: jest.fn((lat, lon) => ({
+      result: {
+        pollution: {
+          ts: Date.now(),
+          aqius: 37,
+          mainus: 'p2',
+          aqicn: 14,
+          maincn: 'p1',
+        },
+      },
+    })),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PollutionController],
-    }).compile();
+      providers: [PollutionService],
+    })
+      .overrideProvider(PollutionService)
+      .useValue(mockPollutionService)
+      .compile();
 
     controller = module.get<PollutionController>(PollutionController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should get pollution with given coordinate', () => {
+    const lat = 42;
+    const lon = 2;
+    expect(controller.getPollution(lat, lon)).toEqual({
+      result: {
+        pollution: {
+          ts: expect.any(Number),
+          aqius: 37,
+          mainus: 'p2',
+          aqicn: 14,
+          maincn: 'p1',
+        },
+      },
+    });
+
+    expect(mockPollutionService.getPollution).toHaveBeenCalledWith(lat, lon);
   });
 });
